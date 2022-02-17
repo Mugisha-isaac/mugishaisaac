573e9779cccb08ce34a77ae41972d6168e1e3862
Mugisha-isaac <mugishaisaac2001@gmail>
2022-02-17 16:08:11 +0200

    doing a form for buying electricity

---
+++ pages/buy.js
@@ -0,0 +1,16 @@
+
+
+export default function Buy(){
+    return(
+        <div className="bg-form_color" style={{width:'25em', marginLeft:'30em', marginTop:'5em'}} >
+                <p className="text-center">Buy Electricity</p>
+                
+                <form>
+                    <div>
+                        <input type="number" placeholder=""/>
+                    </div>
+
+                </form>
+        </div>
+    )
+}
